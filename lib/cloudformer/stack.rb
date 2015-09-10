0395b08d8be28305caa848f6d5292b08872c3607
Antoine Guy <aguy@viadeoteam.com>
2015-09-10 18:00:22 +0200

    update status

--- lib/cloudformer/stack.rb
+++ lib/cloudformer/stack.rb
@@ -1,5 +1,6 @@
 require 'aws-sdk'
 require 'httparty'
+require 'rainbow'
 
 class Stack
   attr_accessor   :stack, :name, :deployed, :resource
@@ -8,7 +9,7 @@ class Stack
   FAILURE_STATES = ["CREATE_FAILED", "DELETE_FAILED", "UPDATE_ROLLBACK_FAILED", "ROLLBACK_FAILED", "ROLLBACK_FAILED", "UPDATE_ROLLBACK_FAILED"]
   END_STATES     = SUCESS_STATES + FAILURE_STATES
 
-  # WAITING_STATES = ["CREATE_IN_PROGRESS","DELETE_IN_PROGRESS","ROLLBACK_IN_PROGRESS","UPDATE_COMPLETE_CLEANUP_IN_PROGRESS","UPDATE_IN_PROGRESS","UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS","UPDATE_ROLLBACK_IN_PROGRESS"]
+  WAITING_STATES = ["CREATE_IN_PROGRESS","DELETE_IN_PROGRESS","ROLLBACK_IN_PROGRESS","UPDATE_COMPLETE_CLEANUP_IN_PROGRESS","UPDATE_IN_PROGRESS","UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS","UPDATE_ROLLBACK_IN_PROGRESS"]
 
   # Config options
   # {:aws_access_key => nil, :aws_secert_access_key => nil, :region => nil}
@@ -19,7 +20,6 @@ class Stack
     @resource = Aws::CloudFormation::Resource.new(client: @cf)
     @stack = @resource.stack(@name) 
     @ec2 = Aws::EC2::Client.new region: config[:region]
-
   end
 
   
@@ -61,7 +61,7 @@ class Stack
         pending_operations = create(template_body, parameters, disable_rollback, capabilities, notify, tags)
       end
     rescue Aws::CloudFormation::Errors::ServiceError => e
-      puts e.message
+      puts Rainbow(e.message).red
       return (if e.message == "No updates are to be performed." then :NoUpdates else :Failed end)
     end
     wait_until_end if pending_operations
@@ -81,7 +81,6 @@ class Stack
         "valid" => false,
         "response" => e.message
       }
-  
     end
   end
 
@@ -98,7 +97,7 @@ class Stack
   end
 
   def create(template_body, parameters, disable_rollback, capabilities, notify, tags)
-    puts "Initializing stack creation..."
+    puts Rainbow("---> Initializing stack creation...").yellow
     template_options = {:template_body => template_body }
     options = {
       :stack_name => name,
@@ -113,8 +112,9 @@ class Stack
   end
 
   def deploy_succeded?
-    return true unless FAILURE_STATES.include?(status_message)
-    puts "Unable to deploy template. Check log for more information."
+    return true if SUCESS_STATES.include?(status_message)
+    return true if WAITING_STATES.include?(status_message)
+    puts Rainbow("--> Unable to deploy template. Check log for more information.").red
     false
   end
 
@@ -127,45 +127,37 @@ class Stack
   end
 
   def delete
-    with_highlight do
-      puts "Attempting to delete stack - #{name}"
-      stack.delete
-      wait_until_end
-      return deploy_succeded?
-    end
+    puts Rainbow("---> Attempting to delete stack - #{name}").yellow
+    stack.delete
+    wait_until_end
+    return deploy_succeded?
   end
 
   def status
-    with_highlight do
-      if deployed
-        puts "#{stack.name} - #{stack.stack_status} - #{stack.stack_status_reason}"
-      else
-        puts "#{stack.name} - Not Deployed"
-      end
+    if deployed
+      puts Rainbow("    #{stack.name} - #{stack.stack_status} - #{stack.stack_status_reason}").green
+    else
+      puts Rainbow("    #{stack.name} - Not Deployed").red
     end
   end
 
   def events(options = {})
-    with_highlight do
-      if !deployed
-        puts "Stack not up."
-        return
-      end
-      stack.events.sort_by {|a| a.timestamp}.each do |event|
-        puts "#{event.timestamp} - #{event.physical_resource_id.to_s} - #{event.logical_resource_id} - #{event.resource_type} - #{event.resource_status} - #{event.resource_status_reason.to_s}"
-      end
+    if !deployed
+      puts Rainbow("---> Stack not up.").red
+      return
+    end
+    stack.events.sort_by {|a| a.timestamp}.each do |event|
+      puts Rainbow("#{event.timestamp} - #{event.physical_resource_id.to_s} - #{event.logical_resource_id} - #{event.resource_type} - #{event.resource_status} - #{event.resource_status_reason.to_s}").yellow
     end
   end
 
   def outputs
-    with_highlight do
     if !deployed
-      puts "Stack not up."
+      puts Rainbow("Stack not up.").red
       return 1
     end
-      stack.outputs.each do |output|
-        puts "#{output.output_key} - #{output.description} - #{output.output_value}"
-      end
+    stack.outputs.each do |output|
+      puts Rainbow("#{output.output_key} - #{output.description} - #{output.output_value}").yellow
     end
     return 0
   end
@@ -174,41 +166,32 @@ class Stack
   def wait_until_end
     printed = []
     current_time = Time.now
-    with_highlight do
-      if !deployed
-        puts "Stack not up."
-        return
-      end
-      loop do
-        printable_events = stack.events.reject{|a| (a.timestamp < current_time)}.sort_by {|a| a.timestamp}.reject {|a| a if printed.include?(a.event_id)}
-        printable_events.each { |event| puts "#{event.timestamp} - #{event.physical_resource_id.to_s} - #{event.resource_type} - #{event.resource_status} - #{event.resource_status_reason.to_s}" }
-        printed.concat(printable_events.map(&:event_id))
-        break if END_STATES.include?(status_message)
-        sleep(10)
-      end
+    if !deployed
+      puts Rainbow("Stack not up.").red
+      return
+    end
+    loop do
+      sleep(3)
+      puts Rainbow("--> events loop : #{stack.stack_status}").blue
+      printable_events = stack.events.reject{|a| (a.timestamp < current_time)}.sort_by {|a| a.timestamp}.reject {|a| a if printed.include?(a.event_id)}
+      printable_events.each { |event| puts Rainbow("---> #{event.timestamp} - #{event.physical_resource_id.to_s} - #{event.resource_type} - #{event.resource_status} - #{event.resource_status_reason.to_s}").yellow }
+      printed.concat(printable_events.map(&:event_id))
+      break if END_STATES.include?(stack.stack_status)
+      sleep(10)
     end
   end
 
-  def with_highlight &block
-    cols = `tput cols`.chomp!.to_i
-    puts "="*cols
-    yield
-    puts "="*cols
-  end
-
   def update_instances(action)
-    with_highlight do
-      puts "Attempting to #{action} all ec2 instances in the stack #{stack.name}"
-      return "Stack not up" if !deployed
-      stack.resources.each do |resource|
-        begin
-          next if resource.resource_type != "Aws::EC2::Instance"
-          physical_resource_id = resource.physical_resource_id
-          puts "Attempting to #{action} Instance with physical_resource_id: #{physical_resource_id}"
-          @ec2.instances[physical_resource_id].send(action)
-        rescue
-          puts "Some resources are not up."
-        end
+    puts Rainbow("---> Attempting to #{action} all ec2 instances in the stack #{stack.name}").yellow
+    return "Stack not up" if !deployed
+    stack.resources.each do |resource|
+      begin
+        next if resource.resource_type != "Aws::EC2::Instance"
+        physical_resource_id = resource.physical_resource_id
+        puts Rainbow("    Attempting to #{action} Instance with physical_resource_id: #{physical_resource_id}").yellow
+        @ec2.instances[physical_resource_id].send(action)
+      rescue
+        puts Rainbow("---> Some resources are not up.").red
       end
     end
   end
